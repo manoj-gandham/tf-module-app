d9e4870489a835aea79f7a63210e3bd0a1bc3049
manoj <gandhamanojkutty@gmail.com>
2024-03-19 23:07:57 +0530

    code updated

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "sg" {
   name        = "${var.name}-${var.env}-sg"
   description = "${var.name}-${var.env}-sg"
-  vpc_id      = var.allow_app_cidr
+  vpc_id      = var.vpc_id
 
   ingress {
     description      = "app"
