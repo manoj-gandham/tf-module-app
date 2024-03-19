0971120c459d252b187b6931ba91619df2c31525
manoj <gandhamanojkutty@gmail.com>
2024-03-19 22:34:18 +0530

    code updated

--- main.tf
+++ main.tf
@@ -46,7 +46,8 @@ resource "aws_security_group" "sg" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-
-  tags = "${var.name}-${var.env}-sg"
+  tags = {
+    name = "${var.name}-${var.env}-sg"
+  }
 }
 
