98c76c884c64bf59d76a1e0ff373e995d7157693
manoj <gandhamanojkutty@gmail.com>
2024-03-20 00:43:46 +0530

    code updated

--- main.tf
+++ main.tf
@@ -1,33 +1,9 @@
-resource "aws_security_group" "sg" {
-  name        = "${var.name}-${var.env}-sg"
-  description = "${var.name}-${var.env}-sg"
-  vpc_id      = var.vpc_id
+resource "aws_instance" "test" {
+  ami = "ami-0d7a109bf30624c99"
+  instance_type = var.instance_type
+  subnet_id = var.subnet_id
 
-  ingress {
-    description = "APP"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = var.allow_app_cidr
-  }
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = var.bastion_cidr
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "${var.name}-${var.env}-sg"
-  }
 }
+
+variable "instance_type" {}
+variable "subnet_id" {}
