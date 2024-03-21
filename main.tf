f1393a994537e41e735b2f4bc8a4b53a27b88420
manoj <gandhamanojkutty@gmail.com>
2024-03-21 22:05:55 +0530

    code updated

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "aws_autoscaling_group" "asg" {
 
 resource "aws_lb_target_group" "main" {
   name     = "${var.name}-${var.env}-tg"
-  port_port    = var.app_port
+  app_port    = var.app_port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
   tags = merge(var.tags, { Name = "${var.name}-${var.env}-tg" })
