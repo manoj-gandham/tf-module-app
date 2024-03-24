fc652f64ab3175545b4a6b9b9fdcc2673f30ad6f
manoj <gandhamanojkutty@gmail.com>
2024-03-24 19:21:49 +0530

    code updated

--- main.tf
+++ main.tf
@@ -19,6 +19,14 @@ resource "aws_security_group" "sg" {
     cidr_blocks = var.bastion_cidr
   }
 
+  ingress {
+    description = "Pometheus"
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = var.monitor_cidr
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
@@ -54,6 +62,7 @@ resource "aws_autoscaling_group" "asg" {
   min_size            = var.min_size
   vpc_zone_identifier = var.subnet_ids
   target_group_arns   = [aws_lb_target_group.main.arn]
+
 
 
   launch_template {
